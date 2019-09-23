97e20ec06aa67d1f4fa08139b1563fca363296b8
User <User@Users-MacBook-Pro.local>
2019-09-23 15:39:13 +0600

    Этап-1 сделан.Инициализировал проект и создал модель Product,cоздал суперадмина,создал фикстуру.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOISES = [
+    ('other', 'Другое'),
+    ('phone', 'Телефон'),
+    ('shoes', 'Обувь'),
+    ('food', 'Еда'),
+    ('clothes', 'Одежда')
+]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Наименование')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    category = models.CharField(max_length=50, null=False, blank=False, choices=CATEGORY_CHOISES, default='other',
+                                verbose_name='Категория')
+    balance = models.IntegerField(verbose_name='Остаток')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return self.name
